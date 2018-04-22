7f76e25a516a33a487775d5dccd81410eea53a45
ElizabethCh08 <38463205+ElizabethCh08@users.noreply.github.com>
2018-04-22 15:38:57 -0500

    Add files via upload

---
+++ Ejrcicio1/src/Ejrcicio1/Ejrcicio1.java
@@ -0,0 +1,10 @@
+package Ejrcicio1;
+
+public class Ejrcicio1 {
+    public static void main(String args[]){
+        double fahrenheit=12;
+        double celsius;
+        celsius=fahrenheit-32*5/9;
+        System.out.println(celsius);
+    }
+}

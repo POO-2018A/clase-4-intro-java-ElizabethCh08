c9eeb8e2de759191913ff6ccc66c10d0c1b1c521
ElizabethCh08 <38463205+ElizabethCh08@users.noreply.github.com>
2018-04-22 15:41:13 -0500

    Add files via upload

---
+++ Ejercicio5/src/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,59 @@
+package Ejercicio5;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese un numero del 1 al 12");
+        int numero = scan.nextInt();
+
+        if (numero >= 1 && numero <= 12) {
+            switch (numero) {
+                case 1:
+                    System.out.println("Enero");
+                    break;
+                case 2:
+                    System.out.println("Febrero");
+                    break;
+                case 3:
+                    System.out.println("Marzo");
+                    break;
+                case 4:
+                    System.out.println("Abril");
+                    break;
+                case 5:
+                    System.out.println("Mayo");
+                    break;
+                case 6:
+                    System.out.println("Junio");
+                    break;
+                case 7:
+                    System.out.println("Julio");
+                    break;
+                case 8:
+                    System.out.println("Agosto");
+                    break;
+                case 9:
+                    System.out.println("Septiembre");
+                    break;
+                case 10:
+                    System.out.println("Octubre");
+                    break;
+                case 11:
+                    System.out.println("Noviembre");
+                    break;
+                case 12:
+                    System.out.println("Diciembre");
+                    break;
+                default:
+                    break;
+            }
+            }else{
+
+                System.out.println("El numero ingresado es incorrecto. Ingrese un numero entre 1 y 12");
+            }
+        }
+
+    }
+
